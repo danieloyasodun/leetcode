ede0ee7026861eef1843e683bb139de64b2803aa
Daniel Oyasodun <119755350+danieloyasodun@users.noreply.github.com>
2026-01-05 20:35:25 +0000

    finished problems

---
+++ leetcode_top_150/array_string/leetcode_274.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def hIndex(self, citations):
+        """
+        :type citations: List[int]
+        :rtype: int
+        """
+        n = len(citations)
+        paper_counts = [0] * (n + 1)
+        
+        for c in citations:
+            paper_counts[min(n, c)] += 1
+        
+        h = n
+        papers = paper_counts[h]
+
+        while papers < h:
+            h -= 1
+            papers += paper_counts[h]
+
+        return h
---
+++ leetcode_top_150/two_pointer/leetcode_125.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        new = []
+        for i in s:
+            if i.isalnum():
+                new.append(i.lower())
+        return new == new[::-1]
+        
