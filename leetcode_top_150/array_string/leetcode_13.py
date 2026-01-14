b5d54ea6791d79be8a526f5f467c472cc461e0cf
Daniel Oyasodun <119755350+danieloyasodun@users.noreply.github.com>
2026-01-14 10:31:34 -0500

    Implement romanToInt function to convert Roman numerals

---
+++ leetcode_top_150/array_string/leetcode_13.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        roman_map = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+
+        result = roman_map[s[-1]]
+        for i in range(len(s) - 2, -1, -1):
+            if roman_map[s[i]] < roman_map[s[i + 1]]:
+                result -= roman_map[s[i]]
+            else:
+                result += roman_map[s[i]]
+
+        return result
