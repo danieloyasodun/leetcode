48f984b65df2b74864bb682c7111eadae344cd14
Daniel Oyasodun <119755350+danieloyasodun@users.noreply.github.com>
2026-01-29 07:37:42 -0500

    Add partition function for linked list
    Implement partitioning of a linked list around a value.

---
+++ leetcode_top_150/linked_list/leetcode_86.py
@@ -0,0 +1,27 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def partition(self, head, x):
+        """
+        :type head: Optional[ListNode]
+        :type x: int
+        :rtype: Optional[ListNode]
+        """
+        left, right = ListNode(), ListNode()
+        ltail, rtail = left, right
+
+        while head:
+            if head.val < x:
+                ltail.next = head
+                ltail = ltail.next
+            else:
+                rtail.next = head
+                rtail = rtail.next
+            head = head.next
+        ltail.next = right.next
+        rtail.next = None
+        return left.next
+        
